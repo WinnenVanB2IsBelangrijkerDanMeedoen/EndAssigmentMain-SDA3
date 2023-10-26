f99bad01bdca54fa92be1ef0c5ba97002ca8973c
TeunJac <119425122+TeunJac@users.noreply.github.com>
2023-10-26 11:16:27 +0200

    jbhv

--- SDA3_final_assignment_sol-main/code/LoadingRobot.py
+++ SDA3_final_assignment_sol-main/code/LoadingRobot.py
@@ -6,19 +6,17 @@ class LoadingRobot():
     def __init__(self):
         super().__init__()
     
-    def TrimFrame():
-        resizedFrame = ResizeFrame()
-        return resizedFrame
-        
+    def TrimFrame(self):
+        self.resizedFrame = ResizeFrame()
 
     def PickUp():
         pass
 
     def PlaceDown():
         pass
-    def PickupPlaceDetection(resizedFrame):
+    def PickupPlaceDetection(self, resizedFrame):
         vidCapture = cv2.VideoCapture(2, cv2.CAP_DSHOW)
-        centerList, frame = ObjectDetection(resizedFrame)
+        centerList, frame = ObjectDetection(self.resizedFrame)
         cv2.imshow("VideoFeed", frame)
         pass
     def CoordinateCalculation():
